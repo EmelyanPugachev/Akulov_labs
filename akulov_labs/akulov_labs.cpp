5e80013250f57121996ff7c60d4fc042325134d9
EmelyanPugachev <akula.vladik@gmail.com>
2023-10-01 19:56:48 +0300

    Summary

---
+++ akulov_labs/akulov_labs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+void menu(){
+	cout << "1.Add tube\n";
+	cout << "2.Add Compressor station\n";
+	cout << "3.View all objects\n";
+	cout << "4.Edit tube\n";
+	cout << "5.Edit Compressor station\n";
+	cout << "6.Save\n";
+	cout << "7.Load\n";
+}
+int main(){
+	menu();
+}
